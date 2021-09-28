415d3963e26da9fdc6b40d5de4b261c4573d4cfc
Stephen George <stephengeorge2640@gmail.com>
2021-09-29 01:12:23 +0530

    Tenth-Commit

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,4 +1,25 @@
-const CartScreen = () => {
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap';
+import Message from '../components/Message';
+import {addToCart} from '../actions/cartActions.js'
+
+
+const CartScreen = (props) => {
+    const productId=props.match.params.id
+    const qty = props.location.search ? Number(props.location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state=> state.cart)
+    const {cartItems} = cart
+    console.log(cartItems)
+    useEffect(() =>{
+        if(productId){
+            dispatch(addToCart(productId,qty))
+        }
+    },[dispatch, productId, qty])
     return (
         <div>
             Cart
