29f442e53eec4a649287eafb935952b2fdcf845e
Stephen George <stephengeorge2640@gmail.com>
2021-11-11 11:41:05 +0530

    Prepare For Deployment

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,7 @@ connectDB()
 
 app.use(express.json())
 
-app.get('/', (req,res) =>{
 
-})
 
 
 
@@ -41,6 +39,19 @@ app.get('/api/config/paypal',(req,res)=> res.send(process.env.PAYPAL_CLIENT_ID))
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use('/carousel_uploads', express.static(path.join(__dirname, '/carousel_uploads')))
+
+
+if(process.env.NODE_ENV === 'production'){
+     app.use(express.static(path.join(__dirname,'/frontend/build')))
+     app.get('*',(req,res)=>{
+         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+     })
+}else{
+    app.get('/', (req,res) =>{
+            console.log("API is Running !")
+    })
+}
+
 
 app.use(notFound)
 app.use(errorHandler)
