80bc6368f7ab43633c84c45a12f3446e5505197f
Stephen George <stephengeorge2640@gmail.com>
2021-11-11 14:24:59 +0530

    Deployment Issues

--- backend/server.js
+++ backend/server.js
@@ -42,9 +42,9 @@ app.use('/carousel_uploads', express.static(path.join(__dirname, '/carousel_uplo
 
 
 if(process.env.NODE_ENV === 'production'){
-     app.use(express.static(path.join(__dirname,'..','frontend','build')))
+     app.use(express.static(path.join(__dirname,'/frontend/build')))
      app.get('*',(req,res)=>{
-         res.sendFile(path.join(__dirname,'..','frontend','build','index.html'))
+         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
      })
 }else{
     app.get('/', (req,res) =>{
